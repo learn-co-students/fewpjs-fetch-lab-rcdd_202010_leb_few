4365daa36711e8ec66f875cbf92863141f589958
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 22:38:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function fetchBooks() {
-
+function fetchBooks(){
+  return fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(data => renderBooks(data))
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main')
+function renderBooks(books){
+  let main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2')
+    let h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
   })
 }
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded',() => {
   fetchBooks()
 })
